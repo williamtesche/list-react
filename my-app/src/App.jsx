560849bec2afde05e19a17ffbebdc5d54dbb9a0e
Willian Matheus Ribeiro Tesche <85637800+williamtesche@users.noreply.github.com>
2021-10-19 16:51:40 -0300

    .

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
 
 
@@ -23,6 +24,7 @@ const App = () => {
     
       <>
         <div className='container'>
+          <AddTask />
         <Tasks tasks={tasks} />
         </div>
         
